abefe74262efabb5d31e2138d5404c5b83257ae3
Tiagodev <tiagodev@protonmail.com>
2021-07-19 10:40:13 -0700

    adding sass and home comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import "./App.scss";
+import Home from "./home/Home";
 
 const App = () => {
-  return <div className="App">Hello word!</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 };
 
 export default App;
