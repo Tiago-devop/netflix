7afa711fd9b8e3502dea82ae475e00fa1ab75d5b
Tiagodev <tiagodev@protonmail.com>
2021-07-27 13:45:30 -0700

    adding list content and styling

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,7 +1,38 @@
+import {
+  Add,
+  PlayArrow,
+  ThumbDownOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
 import "./ListItem.scss";
 
 const ListItem = () => {
-  return <div className="listItem">item</div>;
+  return (
+    <div className="listItem">
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownOutlined />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 min</span>
+          <span className="limit">+16</span>
+          <span>2001</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam iusto
+          repellendus omnis hic laboriosam modi autem porro.
+        </div>
+        <div className="genre">Action</div>
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
